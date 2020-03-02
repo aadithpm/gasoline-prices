497fb8a974af4e770187ce737bc1662cc606319a
aadithpm <aadithpm@gmail.com>
2020-03-01 20:08:31 -0600

    Removing view logic from code

--- app.js
+++ app.js
@@ -4,13 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/prices');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
