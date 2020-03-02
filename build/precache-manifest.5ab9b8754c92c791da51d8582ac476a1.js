20df5ac7a3be97a8507169b26ce840e1f609b39c
aadithpm <aadithpm@gmail.com>
2020-03-02 00:16:44 -0600

    Bundled front-end build

---
+++ build/precache-manifest.5ab9b8754c92c791da51d8582ac476a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497e808cb940c073d23b3ba1db703bee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ffb096be5d513f95466",
+    "url": "/static/css/main.e6898d5c.chunk.css"
+  },
+  {
+    "revision": "75b5ee4e754fe7ebecfc",
+    "url": "/static/js/2.fb1f22ae.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.fb1f22ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ffb096be5d513f95466",
+    "url": "/static/js/main.e82d5c9d.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
